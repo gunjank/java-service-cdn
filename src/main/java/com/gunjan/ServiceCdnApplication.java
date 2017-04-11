4b4c6e681f3c4362425896d111329c5ed504b013
gunjank <sngunjan@yahoo.com>
2017-04-10 20:29:50 -0400

    initial base code

---
+++ src/main/java/com/gunjan/ServiceCdnApplication.java
@@ -0,0 +1,12 @@
+package com.gunjan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceCdnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceCdnApplication.class, args);
+	}
+}
